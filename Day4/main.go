6cc9645bc6ab5f2bd98a55003fa713738bef1ce4
Vili Saura <vili.saura@student.tut.fi>
2021-12-04 13:59:17 +0200

    Day4 part one

---
+++ Day4/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+  "os"
+  "strings"
+  "strconv"
+  "regexp"
+)
+
+
+type bingoBoard struct {
+  solved bool
+  boardValues [5][]string
+}
+
+var boards []bingoBoard
+
+func main() {
+  parsedData := parseData()
+  incomingValuestemp := parsedData[0]
+  incomingValues := strings.Split(string(incomingValuestemp), ",")
+  bingoBoards := parsedData[1:]
+  createBingoBoards(bingoBoards)
+
+  for _, value := range incomingValues{
+    checkboards(string(value))
+  }
+}
+
+func parseData() []string{
+  inputValues, _ := os.ReadFile("input.txt")
+  separateValues := strings.Split(string([]byte(inputValues)),"\r\n\r\n")
+  return separateValues
+}
+
+
+func createBingoBoards(values []string){
+
+  for _, inputBoard := range values{
+    var tempboard [5][]string
+    valuesParsed := strings.Split(inputBoard,"\r\n")
+
+    for i, row := range valuesParsed{
+      space := regexp.MustCompile(`\s+`)
+      tempRow := space.ReplaceAllString(row, " ")
+      rowCleaned := strings.TrimLeft(tempRow, " ")
+      rowParsed := strings.Split(rowCleaned," ")
+      tempboard[i] = (rowParsed)
+    }
+
+    boards = append(boards, bingoBoard{solved:false, boardValues:tempboard})
+
+  }
+}
+
+func checkboards(value string){
+  for _, board := range boards{
+    for i, row := range board.boardValues{
+      for j, boardValue := range row{
+        if boardValue == value{
+          board.boardValues[i][j] = "X"
+          winValue, _ := strconv.Atoi(value)
+          checkWin(board.boardValues, row, j, winValue)
+        }
+      }
+    }
+  }
+}
+
+func checkWin(boardToCheck [5][]string, row []string, index int, winningValue int){
+
+    rowWin := 0
+    columnWin := 0
+    for _, value := range row{
+      if value == "X"{
+        rowWin ++
+      }
+    }
+    if rowWin == 5{
+      fmt.Println("Voitto")
+      fmt.Println(boardToCheck)
+      calculateWin(boardToCheck, winningValue)
+    }
+    for _, row := range boardToCheck{
+      if row[index] == "X"{
+        columnWin ++
+      }
+    }
+    if columnWin == 5{
+      fmt.Println("Voitto")
+      fmt.Println(boardToCheck)
+      calculateWin(boardToCheck, winningValue)
+    }
+}
+
+func calculateWin(boardToCheck [5][]string, winningValue int){
+  winSum := 0
+  for _, row := range boardToCheck{
+    for _, boardValue := range row{
+      if boardValue != "X"{
+        number,_ :=  strconv.Atoi(boardValue)
+        winSum += number
+      }
+    }
+  }
+  fmt.Println(winSum * winningValue)
+  os.Exit(3)
+}
