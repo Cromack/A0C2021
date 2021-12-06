1668c8a9a3121042836d6dbbe6f4b6b57d66ef12
Vili Saura <vili.saura@student.tut.fi>
2021-12-06 13:30:44 +0200

    Day6 completed

--- Day6/main.go
+++ Day6/main.go
@@ -10,44 +10,44 @@ import (
 var amountOfDays = 256
 var newCycle = 8
 var existingCycle = 6
-type point struct{
-  x,y int
-}
 
 func main() {
-  parsedData := parseData()
-	fishStates := convertData(parsedData)
+	fishStates := convertData(parseData())
   for days := 0; days < amountOfDays; days ++{
-    increaseCycle(&fishStates)
-		//fmt.Println((fishStates))
+    fishStates = increaseCycle(fishStates)
 
   }
-	fmt.Println(len(fishStates))
+	fishSum := 0
+	for _, v := range fishStates{
+		fishSum = fishSum + v
+	}
+	fmt.Println(fishSum)
 }
 
 func parseData() []string{
-  inputValues, _ := os.ReadFile("testInput.txt")
+  inputValues, _ := os.ReadFile("input.txt")
   separateValues := strings.Split(string([]byte(inputValues)),",")
   return separateValues
 }
 
-func convertData(rawData []string) []int{
-	converted := make([]int, len(rawData))
-	for i, value := range rawData{
+func convertData(rawData []string) map[int]int{
+	converted := make(map[int]int)
+	for _, value := range rawData{
 		x,_ := strconv.Atoi(value)
-		converted[i] = x
+		converted[x]++
 	}
 	return converted
 }
 
-func increaseCycle(lanternfish *[]int){
-	for i, fish := range *lanternfish{
-		if fish == 0{
-			(*lanternfish)[i] = existingCycle
-			*lanternfish = append(*lanternfish, newCycle)
-		}else{
-			(*lanternfish)[i]--
+func increaseCycle(lanternfish map[int]int)map[int]int {
+	tempMap := make(map[int]int)
+	for i, v := range lanternfish{
+		if i > 0{
+			tempMap[i-1] = tempMap[i-1] + v
+		}else if i == 0{
+			tempMap[existingCycle] = tempMap[existingCycle] + v
+			tempMap[newCycle] = v
 		}
 	}
-
+	return tempMap
 }
